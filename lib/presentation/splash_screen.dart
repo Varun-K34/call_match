b009bc4247c227ef9267104e3cd016971e758bd7
Varun-K34 <appuvarun88@gmail.com>
2024-06-29 15:53:57 +0530

    Splash Update

--- lib/presentation/splash_screen.dart
+++ lib/presentation/splash_screen.dart
@@ -5,6 +5,8 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Column(children: [Center(child: Text("data"))],)
+    );
   }
 }
